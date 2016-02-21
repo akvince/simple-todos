8bdc7436a3a77d560eed9e2e98337e3ac50f7f23
akvince <deflandre.vincent@live.fr>
2016-02-21 12:33:41 +0100

    validate step3 'collections'

--- simple-todos.js
+++ simple-todos.js
@@ -8,3 +8,13 @@ if (Meteor.isClient) {
     ]
   });
 }
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: function () {
+      return Tasks.find({});
+    }
+  });
+}
